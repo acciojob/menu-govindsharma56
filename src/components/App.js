cb07f450068d5f8fcfbf2658c3641930e02b2001
unknown <govindsharmags1057@gmail.com>
2026-09-21 20:39:51 +0530

    menu updated

--- src/components/App.js
+++ src/components/App.js
@@ -22,14 +22,14 @@ const App = () => {
     <div id="main">
        <h2 style={{textAlign:"center",color:'blue'}}>Our Menu</h2>
        <div className='allbutton' style={{marginTop:'10px'}} >
-              <button onClick={()=>{handleClick('all')}}>All</button>
-              <button onClick={()=>{handleClick('lunch')}}>Lunch</button>
-              <button onClick={()=>{handleClick('breakfast')}}>Breakfast</button>
-              <button onClick={()=>{handleClick('shake')}}>Shake</button>
+              <button  onClick={()=>{handleClick('all')}}>All</button>
+              <button id='#filter-btn-2' onClick={()=>{handleClick('lunch')}}>Lunch</button>
+              <button id='#filter-btn-1' onClick={()=>{handleClick('breakfast')}}>Breakfast</button>
+              <button  id='#filter-btn-3'onClick={()=>{handleClick('shake')}}>Shake</button>
        </div>  
            <div className="section-center" style={{marginTop:'40px'}}>
               {display.map((item)=>(
-                            <article className="menu-item " >
+                            <article className="menu-item "  key={item.id} data-test-id={`menu-item-${item.category}`}>
                             <img src={item.img} height={100} width={100}  className="photo" />
                             <div className="item-info">
                                 <header>
